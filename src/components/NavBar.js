ab090fd4322b66e793c12a2669085e94e9c1df4b
Sergio Ardila <sardila535@gmail.com>
2024-01-31 07:02:17 -0500

    link a mi cv reparado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,7 +50,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://issuu.com/sergio-dev/docs/cv-sergio.ardila"><img src={navIcon1} alt="" /></a>
                 <a href="https://github.com/Sergio-AB"><img src={navIcon2} alt="" /></a>
                 <a href="https://wa.link/6csgrr"><img src={navIcon3} alt="" /></a>
               </div>
